eb5957a6c21b0a2c4178294aa5c25142cab7e7de
iHok <ihokya@gmail.com>
2019-12-12 20:19:19 +0900

    ボタンを削除し、入力と選択で表示できるように修正

--- spreadsheetsViewer/js/spreadsheetsScript.js
+++ spreadsheetsViewer/js/spreadsheetsScript.js
@@ -1,34 +1,32 @@
-function readSpreadsheets() {
-  let spreadsheetsURLsplit = document.querySelector("#spreadsheetsURL").value.split("/");
-  var url = "https://spreadsheets.google.com/feeds/worksheets/" + spreadsheetsURLsplit[5] + "/public/basic?alt=json";
-  console.log(url)
-  document.getElementById("sel1").innerHTML = '';
-  fetch(url)
-    .then((response) => response.json())
-    .then((json) => {
-      console.log(json.feed);
-      for (var i = 0; i < json.feed.entry.length; i++) {
-        console.log(json.feed.entry[i].content.$t + ":" + json.feed.entry[i].link[1].href);
-        let op = document.createElement("option");
-        op.value = json.feed.entry[i].link[1].href.split("/")[6];  //value値
-        op.text = json.feed.entry[i].content.$t;   //テキスト値
-        document.getElementById("sel1").appendChild(op);
-      }
-
-
-      return false;
-    });
+const changeSelectLists = (inputValue) => {
+  console.log("changeSelectLists")
+  let inputValueSplit = inputValue.split("/");
+  if (inputValueSplit[2] === "docs.google.com" && inputValueSplit[3] === "spreadsheets") {
+    var url = "https://spreadsheets.google.com/feeds/worksheets/" + inputValueSplit[5] + "/public/basic?alt=json";
+    console.log(url)
+    document.getElementById("sel1").innerHTML = '<option value="">選択して下さい</option>';
+    fetch(url)
+      .then((response) => response.json())
+      .then((json) => {
+        console.log(json);
+        console.log(json.feed.entry.length);
+        for (var i = 0; i < json.feed.entry.length; i++) {
+          let op = document.createElement("option");
+          op.value = inputValueSplit[5]+"/"+json.feed.entry[i].link[1].href.split("/")[6];  //value値
+          op.text = json.feed.entry[i].content.$t;   //テキスト値
+          document.getElementById("sel1").appendChild(op);
+        }
+        return false;
+      });
+   } else {
+    console.log("スプレッドシートのURLではありません")
+    return false;
+  }
 }
 
-function createTable() {
-  const sel1 = document.form1.sel1;
-
-  // 値(数値)を取得
-  const num = sel1.selectedIndex;
-  let spreadsheetsURLsplit = document.querySelector("#spreadsheetsURL").value.split("/");
-
+const createTable = (sheetValue) => {
   // 値(数値)から値(value値)を取得
-  const str = "https://spreadsheets.google.com/feeds/list/" + spreadsheetsURLsplit[5] + "/" + sel1.options[num].value + "/public/values?alt=json";
+  const str = "https://spreadsheets.google.com/feeds/list/" + sheetValue + "/public/values?alt=json";
   console.log(str);
   const result = document.getElementById("result");
   document.getElementById("table").innerHTML = '';
@@ -36,7 +34,7 @@ function createTable() {
   fetch(str)
     .then((response) => response.json())
     .then((json) => {
-      console.log(json);
+      //      console.log(json);
       //        result.innerHTML = json.feed;        
       // console.log(myJson.length);                                               
       for (var i = 0; i < json.feed.entry.length; i++) {
@@ -53,5 +51,5 @@ function createTable() {
 }
 
 window.onload = function () {
-  readSpreadsheets();
-}
+  changeSelectLists(document.querySelector("#spreadsheetsURL").value);
+};
